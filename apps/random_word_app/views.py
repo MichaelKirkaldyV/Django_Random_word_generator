ab1be984f4bdad3951007cad2ab9d80d51847761
Michael <vmichaelkirkaldy@live.com>
2018-07-03 12:23:19 -0500

    updates to views.py

--- apps/random_word_app/views.py
+++ apps/random_word_app/views.py
@@ -10,25 +10,20 @@ def generator(request):
 	#must run the following commands in the terminal to use Sessions. 
 	#python manage.py makemigrations
     #python manage.py migrate
-    request.session['count'] == False
-    if 'count' in request.session == False:
+    if not 'count' in request.session:
     	request.session['count'] = 1
     else:
-    	request.session['count'] +=1
-
-	#creates a random string with a specified length of 14 characters 
-	rand_word = get_random_string(length=14)
+    	request.session['count'] +=1	
 
 	context = {
 		#key must be in quotes
-		"rand_word": rand_word	
+		#creates a random string with a specified length of 14 characters 
+		"rand_word": get_random_string(length=14)
 	}
 	return render(request,'random_word_app/index.html', context)
 
 def reset(request):
-
-	if 'count' in request.session == True:
 		del request.session['count'] 
-	return redirect("/generate")
+		return redirect("/generate")
 
 
